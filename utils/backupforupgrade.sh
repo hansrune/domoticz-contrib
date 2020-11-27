25f4af4cfac30419ca9a0b019728f091ff0235e0
Hans-Rune Bue <email@hansrune.net>
2020-11-27 08:02:39 +0100

    backupforupgrade refactored

--- utils/backupforupgrade.sh
+++ utils/backupforupgrade.sh
@@ -1,9 +1,33 @@
-#!/bin/sh
-PROG=$( basename $0 ) 
-DATE=$( date +"%Y-%m-%d" )
-HOST=$( hostname -s )
-TAR="/tmp/domoticz-${HOST}-${DATE}.tar.gz"
-tar -C /opt/domoticz --exclude="./back*" --exclude="./.n*" --exclude=./.config --exclude=./.cache -czf ${TAR} $* . && echo "$PROG: Saved in $TAR" && exit 0
+#!/bin/bash
+#
+# Backup domoticz code before upgrades
+#
+PROG=$( basename $0 .sh )
+DIR=$( dirname $0 ) 
+HOST=$( hostname -s ) 
+TIMESTR=$( date +"%Y-%m-%d" )
+BACKUPDIR="backups/domoticz"
+TARFILE="${BACKUPDIR}/domoticz-${HOST}-${TIMESTR}.tar.gz"
+
+if [ ! -d ${BACKUPDIR} ]
+then
+	echo "$PROG: No ${BACKUPDIR} directory"
+	exit 2
+fi
+if [ ! -f "${DIR}/backupexcludes.txt" ]
+then
+	echo "$PROG: No excludeslist file in ${DIR}/backupexcludes.txt" >&2
+	exit 2
+fi
+if [ ! -x "domoticz" ]
+then
+	echo "$PROG: No domoticz executable in current directory" >&2
+	exit 2
+fi
+echo "Backup to ${TARFILE}"
+tar -X ${DIR}/backupexcludes.txt -czf ${TARFILE} * 
+
 RC=$?
-echo "$PROG: Failed ? Return code $RC"
-exit $RC
+[ ${RC} -ne 0 ] && echo "$PROG: Failed ? Return code $RC" >&2
+#
+# vim:ts=4:sw=4
